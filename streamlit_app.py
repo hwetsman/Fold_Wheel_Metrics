9f339670c3b1c4e87cfbc899059d9cdb3d8e4165
hwetsman <howard@tocdr.com>
2022-12-19 13:22:22 -0600

    Refactor: create streamlit app scaffolding

---
+++ streamlit_app.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# set active spins file
+file = 'Holidays_2022_Wheel.csv'
+# file = 'September_Premium_Wheel.csv'
+# file = 'Summer_Wheel_2.csv'
+# file = 'Summer_Wheel.csv'
+# file = 'Revamped_June_Wheel.csv'
+# file = 'June_Wheel.csv'
+# file = 'Late_May_Wheel.csv'
+# file = 'May_Wheel.csv'
+# file = 'Easter_Wheel.csv'
+# file = 'March_April.csv'
+# file = 'March_Renew_Wheel.csv'
+# file = 'March_Wheel.csv'
+# file = 'February_Wheel.csv'
+# file = 'New_Year_Wheel.csv'
+
+# get active expecteds file
+expected_df = pd.read_csv('Expected_Rewards.csv')
+# print(expected_df)
+col_name, _ = file.split('.')
+expected_df = expected_df[['Award', col_name]].dropna()
+expected_df.rename(columns={col_name: 'Exp_Freq'}, inplace=True)
+# print(f'Expected rewards for {col_name}\n')
+
+# get current spins
+df = pd.read_csv(file)
+print(df)
+observations = df.shape[0]
+print(f'The first 5 of the list of rewards of {observations} spins')
+obs_spins = df.SPIN_RESULT.tolist()
+obs_spins[0:5]
+spins_set_list = list(set(obs_spins))
+print(spins_set_list)
+
+# create dataframe of observed spins frequency
+observed_df = pd.DataFrame(pd.DataFrame(
+    {'Result': obs_spins}).value_counts(normalize=True), columns=['Freq'])
+observed_df['Type'] = 'Obs_Freq'
+observed_df.reset_index(inplace=True, drop=False)
+observed_df = observed_df[observed_df.Result.isin(expected_df['Award'].tolist())]
+observed_df
+
+# combine expected and observed
+expected_df
+expected_df.rename(columns={'Award': 'Result', 'Exp_Freq': 'Freq'}, inplace=True)
+exp_set_list = expected_df.Result.tolist()
+expected_df['Type'] = 'Expected_Freq'
+expected_df = expected_df[expected_df.Result != 'Total']
+# removes any expected spin that didn't occur
+expected_df = expected_df[expected_df.Result.isin(list(set(obs_spins)))]
+# expected_df.Freq = expected_df.Freq.astype(float)
+observed_df = observed_df[observed_df['Result'].isin(exp_set_list)]
+df = expected_df.append(observed_df, ignore_index=True)
+print(df)
+
+# intuititve first look
+sns.set(rc={'figure.figsize': (11, 5)})
+sns.set_style('white')
+sns.pointplot(x=df.Type, y=df.Freq, hue=df.Result, data=df, dodge=True)
+
+# bootstrapping
+print('Green lines are 95% confidence intervals of the distributions of the distribution in blue')
+print('Blue line is observed frequency. Black line is expected frequency')
+exp_set_list = [x for x in exp_set_list if x in observed_df.Result.tolist()]
+spins = np.array(obs_spins)
+for award in exp_set_list:
+    expected = expected_df[expected_df.Result == award].reset_index()['Freq'][0]
+    temp = observed_df[observed_df.Result == award].reset_index()
+    obs_freq = temp.Freq.tolist()[0]
+    award_freqs = []
+    for i in range(10000):
+        # create bootstrap sample
+        boot = np.random.choice(spins, spins.size, replace=True)
+        # find resulting frequency for 30-day purchases
+        award_freq = boot[boot == award].size/boot.size
+        award_freqs.append(award_freq)
+    plt.figure(figsize=(8, 5))
+    array = np.array(award_freqs)
+    tile_25 = np.percentile(array, 2.5)
+    tile_975 = np.percentile(array, 97.5)
+    plt.axvline(tile_25, color='green')
+    plt.axvline(tile_975, color='green')
+    plt.axvline(expected, color='black')
+    plt.title(f'Bootstrapped Distribution of {award} Reward. Expected: {expected}')
+    plt.hist(award_freqs)
+
+    plt.axvline(obs_freq, color='b')
+    plt.show()
