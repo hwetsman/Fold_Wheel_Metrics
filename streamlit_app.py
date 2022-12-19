bf3d16acda8721bdd7ccabd55fea9c373236eff7
hwetsman <howard@tocdr.com>
2022-12-19 14:28:35 -0600

    Refactor: remove sns dependence

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
