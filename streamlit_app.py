a032f4c9a4680fcf99df9699e1f49bdad739c5ce
hwetsman <howard@tocdr.com>
2022-12-30 07:58:28 -0600

    Refactor: Change tick angle

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ fig = px.line(df, y="Freq", x="Type", color="Result",
               title="Difference between Expected and Observed Frequency of Observed Rewards")  # , symbol="medal")
 fig.update_traces(marker_size=10)
 fig.update_layout(title_x=0.5)
-fig.update_xaxes(type='category', title_text="", tickfont=dict(
+fig.update_xaxes(type='category', title_text="", tickangle=90, tickfont=dict(
     family='Rockwell', color='crimson', size=14))
 st.plotly_chart(fig, use_container_width=True)
 
