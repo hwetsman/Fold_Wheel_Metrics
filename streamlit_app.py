da27a83cd098f5506f99aedff6267b8aa0034699
hwetsman <howard@tocdr.com>
2022-12-30 07:47:49 -0600

    Refactor: try catigorical x axis

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ fig = px.line(df, y="Freq", x="Type", color="Result",
               title="Difference between Expected and Observed Frequency of Observed Rewards")  # , symbol="medal")
 fig.update_traces(marker_size=10)
 fig.update_layout(title_x=0.5)
-fig.update_xaxes(type='category', title_text="")
+fig.update_xaxes(type='category', title_text="", title_font={"size": 20})
 st.plotly_chart(fig, use_container_width=True)
 
 # bootstrapping
